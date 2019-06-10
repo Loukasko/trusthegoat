86c6a3e762468ea9050e7d465bf6032c6166df0a
Loukasko <loukasko24@gmail.com>
2019-06-10 23:38:14 +0300

    delta

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1560802990;s:7:"authKey";s:64:"n%MOUfv>YX?l^<jHTN{IjK[T7QXS6!F{5[DLuT;j)<1?(h3r0tNORN)15EJCb[{m";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2121;}
